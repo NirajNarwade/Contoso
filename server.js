586dd38b48bf947a2fe6d6c6d10b5909fc8ab327
NirajNarwade <narwade.niraj.44@gamil.com>
2022-01-29 10:47:39 +0530

    contosoapp

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var express =require("express");  
 var app=express(); 
-/*
-var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
-              "<hr/>"+
-              "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
-             "</ol>");
-};
-*/
+
 app.all('*',function(req,res){
     res.sendfile(__dirname+"/index.html");
 });
